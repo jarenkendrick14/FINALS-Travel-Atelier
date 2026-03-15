a7b150fdc7563332fbe7e110621588ea3fad81e5
Jaren Kendrick <jarenkendricky@gmail.com>
2026-03-16 06:40:09 +0800

    Fix: scroll to top on every route navigation

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import MyBookingsView from '../views/MyBookingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: () => ({ top: 0, behavior: 'smooth' }),
   routes: [
     {
       path: '/',
